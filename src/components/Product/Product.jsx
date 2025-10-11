f49266e093cbf5683f3be286f952c3e72d2fbba5
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-10-10 18:08:25 -0700

    Updated Product component

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,11 +1,46 @@
-import { useParams } from "react-router";
+import { useOutletContext, useParams } from "react-router";
 import styles from "./Product.module.css";
 function Product() {
   const { name } = useParams();
+  const { products } = useOutletContext();
+
+  const item = products.find((product) => product.title === name);
+
   return (
-    <div className={styles.productContainer}>
-      <h1>{name}</h1>
-    </div>
+    <>
+      {item ? (
+        <div className={styles.itemContainer}>
+          <div className={styles.itemWrapper}>
+            <div className={styles.imageWrapper}>
+              <img src={item.image} alt="product image" />
+            </div>
+
+            <div className={styles.itemInfoWrapper}>
+              <div className={styles.itemInfo}>
+                <div className={styles.itemTitle}>
+                  <h1>{item.title}</h1>
+                </div>
+                <div className={styles.itemReview}>
+                  <span>No reviews found</span>
+                </div>
+                <div className={styles.itemPrice}>
+                  <span>${item.price}</span>
+                </div>
+                <div className={styles.itemQuantityWrapper}>
+                  <div className={styles.itemQuantity}>
+                    <button className="decrease">-</button>
+                    <input type="number" min="1" value="1" />
+                    <button className="increase">+</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <h1></h1>
+      )}
+    </>
   );
 }
 
